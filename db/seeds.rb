03b079955b7d3bc08ccb658ec95f9138627bb4aa
Igor Plotnikov <ig.plotnikov@gmail.com>
2024-01-12 00:20:05 +0300

    Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+30.times do
+  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+  client = Faker::University.name
+  amount = Faker::Commerce.price(range: 10000.0..100000.0, as_string: true)
+  comment = Faker::Hipster.sentence(word_count: 10)
+
+  Offer.create(body: body, client: client, amount: amount, comment: comment)
+end
