80a16e2a570b5766503060f4958ed0bdbb3ea302
Igor Plotnikov <ig.plotnikov@gmail.com>
2024-01-12 01:25:16 +0300

    refactor pagy initializer

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -5,4 +5,5 @@ Pagy::DEFAULT[:items] = 25       # items per page
 Pagy::DEFAULT[:size]  = [1,4,4,1] # nav bar links
 # Better user experience handled automatically
 require 'pagy/extras/overflow'
+
 Pagy::DEFAULT[:overflow] = :last_page
